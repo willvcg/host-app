67c6e71812a712473b9dae62846ada6afb753ea3
Will Cumbicus <wilmanvc93@gmail.com>
2025-06-14 12:16:11 +0200

    Add routes for micro-frontends

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'micro-frontend-1',
+        loadComponent: () => loadRemoteModule('micro-frontend-1', './Component').then(m => m.AppComponent)
+    },
+    {
+        path: 'micro-frontend-2',
+        loadComponent: () => loadRemoteModule('micro-frontend-2', './Component').then(m => m.AppComponent)
+    }
+];
